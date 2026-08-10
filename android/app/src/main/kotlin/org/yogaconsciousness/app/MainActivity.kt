577f26ad9557f5db1eec36637e15dcfc5624ad05
raparty <59390435+raparty@users.noreply.github.com>
2026-08-10 11:03:05 +0530

    fix: revert MainActivity, remove androidx.core, bump to 1.4.4+12

--- android/app/src/main/kotlin/org/yogaconsciousness/app/MainActivity.kt
+++ android/app/src/main/kotlin/org/yogaconsciousness/app/MainActivity.kt
@@ -1,15 +1,5 @@
 package org.yogaconsciousness.app
 
-import android.os.Build
-import android.os.Bundle
-import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
-            WindowCompat.setDecorFitsSystemWindows(window, false)
-        }
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity: FlutterActivity()
